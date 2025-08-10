5f187582c0067632e3d25a6c6ecf22009820c1aa
Shahryar-Sohail <shahryar.ch123@gmail.com>
2025-08-10 22:57:10 +0500

    Login With Email Password Implemented

--- Frontend/src/pages/Login.tsx
+++ Frontend/src/pages/Login.tsx
@@ -1,5 +1,27 @@
+import { useState } from "react"
+import {app} from "../firebase"
+import {getAuth,signInWithEmailAndPassword} from "firebase/auth"
 
 const Login = () => {
+
+    const [email , setEmail] = useState("")
+    const [password , setPassword] = useState("")
+
+    const handleLogin =(e)=>{
+        e.preventDefault();
+        const auth = getAuth(app);
+        signInWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                // Signed in
+                const user = userCredential.user;
+                console.log("Login successful:", user);
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                console.error("Login failed:", errorCode, errorMessage);
+            });
+    }
     return (
         <div>
             <div className="w-full max-w-sm mx-auto overflow-hidden bg-white rounded-lg shadow-md dark:bg-gray-800 p-10">
@@ -9,17 +31,22 @@ const Login = () => {
                     <p className="mt-1 text-center text-gray-500 dark:text-gray-400">Login for Admin</p>
                     <form>
                         <div className="w-full mt-4">
-                            <input className="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-500 bg-white border rounded-lg dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300" type="email" placeholder="Email Address" aria-label="Email Address" />
+                            <input onChange={e=>setEmail(e.target.value)} value={email} 
+                            className="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-500 bg-white border rounded-lg dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300" type="email" placeholder="Email Address" aria-label="Email Address" />
                         </div>
                         <div className="w-full mt-4">
-                            <input className="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-500 bg-white border rounded-lg dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300" type="password" placeholder="Password" aria-label="Password" />
+                            <input onChange={e=>setPassword(e.target.value)} value={password} 
+                            className="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-500 bg-white border rounded-lg dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300" type="password" placeholder="Password" aria-label="Password" />
                         </div>
                         <div className="flex items-center justify-center mt-4">
-                            <button className="px-6 py-2 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-500 rounded-lg hover:bg-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50">
+                            <button onClick={handleLogin} className="px-6 py-2 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-500 rounded-lg hover:bg-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50">
                                 Sign In
                             </button>
                         </div>
                     </form>
+                </div>
+                <div className="flex items-center justify-center py-4 text-center bg-gray-50 dark:bg-gray-700">
+                    <p>Email: admin@gmail.com <br/> Password: admin123</p>
                 </div>
                 {/* <div className="flex items-center justify-center py-4 text-center bg-gray-50 dark:bg-gray-700">
                     <span className="text-sm text-gray-600 dark:text-gray-200">Don't have an account? </span>
